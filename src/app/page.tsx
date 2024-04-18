1f487b2a6af0f0eaaa9e6fe173c376a33ce1ee99
Aleksey Verin <verevaa@gmail.com>
2024-04-18 12:20:58 +0300

    fix: clean code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,14 @@ import styles from './page.module.scss'
 import Header from '@/components/shared/header/header'
 import Causes from '@/components/home/information/causes/causes'
 import Promo from '@/components/home/promo/promo'
+import Information from '@/components/home/information/information'
 
 export default function Home() {
   return (
     <main >
       <Header />
       <Promo />
-      <Causes />
-      <section className={styles.promo}>
-        Атопический дерматит — это аллергическое кожное заболевание, которое имеет наследственную
-        предрасположенность, протекает хронически, сопровождается зудом
-      </section>
-      <section className={styles.app}>
-        Атопический дерматит — это аллергическое кожное заболевание, которое имеет наследственную
-        предрасположенность, протекает хронически, сопровождается зудом
-      </section>
+      <Information />
       <Footer />
     </main>
   )
