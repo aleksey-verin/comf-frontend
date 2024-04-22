f013a9e2963711f6c1d7d2630cc41c8fd42bc692
Aleksey Verin <verevaa@gmail.com>
2024-04-22 17:47:36 +0300

    feat: add single article page without content

---
+++ src/components/articles/articles-item.tsx
@@ -0,0 +1,44 @@
+import './articles-item.scss'
+
+const ArticlesItem = ({
+  title,
+  description,
+  slug,
+  image,
+  imageTablet,
+  imageMobile,
+  twoColumns,
+}: {
+  title: string
+  description: string
+  slug: string
+  image: string
+  imageTablet: string
+  imageMobile: string
+  twoColumns?: boolean
+}) => {
+  return (
+    <li className={`articles-grid__item articles-grid__item--${twoColumns ? 'two' : 'one'}-column`}>
+      <div className='articles-grid__wrap'>
+        <div className='articles-grid__image'>
+          <picture>
+            <source media='(max-width: 767px)' srcSet={imageMobile} />
+            <source media='(max-width: 1279px)' srcSet={imageTablet} />
+            <img src={image} alt={title} />
+          </picture>
+        </div>
+        <div className='articles-grid__text'>
+          <h3 className='articles-grid__title'>{title}</h3>
+          <div className='articles-grid__description'>
+            {description}
+          </div>
+        </div>
+        <a href={`/articles/${slug}`} className='button articles-grid__button'>
+          Узнать больше
+        </a>
+      </div>
+    </li>
+  )
+}
+
+export default ArticlesItem
