85695ec4c7e455f0534e36a1ce2ccc9bc2cb3eaa
Aleksey Verin <verevaa@gmail.com>
2024-04-26 16:34:50 +0300

    feat: change urls

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '1337',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'admin-strapi.vereva.ru',
+        port: '1337',
+        pathname: '/**',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
